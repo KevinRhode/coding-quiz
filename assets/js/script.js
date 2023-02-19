0e4c107d430a3394a676604c45c04795d3a24681
Kevin Rhode <kevinrhode@hotmail.com>
2023-02-19 15:00:07 -0500

    add: js start, button add for answers, overall layout

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,34 @@
 let card = document.createElement("div");
 card.classList.add("quiz-question-card");
 
+//create header of card
+let cardHeader = document.createElement("h2");
+cardHeader.textContent = "Quiz Question of Obj";
+
+card.appendChild(cardHeader);
+
+//create content of card
+
+let cardContent = document.createElement("ol");
+let cardContentItem1 = document.createElement("li");
+cardContentItem1.textContent="Button Here"
+let buttonHit = document.createElement("button");
+// buttonHit.type="button";
+buttonHit.innerText="text answer on the button";
+cardContentItem1.appendChild(buttonHit);
+let cardContentItem2 = document.createElement("li");
+let cardContentItem3 = document.createElement("li");
+let cardContentItem4 = document.createElement("li");
+
+cardContent.appendChild(cardContentItem1);
+cardContent.appendChild(cardContentItem2);
+cardContent.appendChild(cardContentItem3);
+cardContent.appendChild(cardContentItem4);
+
+card.appendChild(cardContent);
+
+let container = document.querySelector(".quiz-question-container")
+container.appendChild(card);
 
 //credits
 //https://stackoverflow.com/questions/507138/how-to-add-a-class-to-a-given-element
