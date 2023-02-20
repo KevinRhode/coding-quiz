b733c7f9083c7ab515713fd21b2c9cc4251703ea
Kevin Rhode <kevinrhode@hotmail.com>
2023-02-20 17:47:19 -0500

    add: close to what I want for style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,27 @@
+let questionObj = {
+    question:"Arrays in JavaScript can be used to store _________.",
+    answers:["A","B","C","D"],
+    answer:"A"
+}
+timerCount =0;
+
+let headerElement = document.querySelector("header");
+let aElement = document.createElement("a");
+aElement.href="#";
+aElement.textContent = "View High Scores";
+let labelElement = document.createElement("label");
+labelElement.textContent = "Time: " + timerCount;
+headerElement.appendChild(aElement);
+headerElement.appendChild(labelElement);
+
+
+
+
+
+
+
+
+
 
 
 //create a card
@@ -6,27 +30,28 @@ card.classList.add("quiz-question-card");
 
 //create header of card
 let cardHeader = document.createElement("h2");
-cardHeader.textContent = "Quiz Question of Obj";
+cardHeader.textContent = questionObj.question;
+// cardHeader.textContent = "Quiz Question of Obj";
 
 card.appendChild(cardHeader);
 
 //create content of card
 
 let cardContent = document.createElement("ol");
-let cardContentItem1 = document.createElement("li");
-cardContentItem1.textContent="Button Here"
-let buttonHit = document.createElement("button");
-// buttonHit.type="button";
-buttonHit.innerText="text answer on the button";
-cardContentItem1.appendChild(buttonHit);
-let cardContentItem2 = document.createElement("li");
-let cardContentItem3 = document.createElement("li");
-let cardContentItem4 = document.createElement("li");
 
-cardContent.appendChild(cardContentItem1);
-cardContent.appendChild(cardContentItem2);
-cardContent.appendChild(cardContentItem3);
-cardContent.appendChild(cardContentItem4);
+
+
+
+for (let index = 0; index < questionObj.answers.length; index++) {
+    
+    let cardContentItem = document.createElement("li");
+    let buttonHit = document.createElement("button");
+    // buttonHit.type="button";
+    buttonHit.innerText=questionObj.answers[index];
+    cardContentItem.appendChild(buttonHit);
+    cardContent.appendChild(cardContentItem);
+    
+}
 
 card.appendChild(cardContent);
 
